a7b57c776c06d1ff85037509a871c25e7ff17d88
Nisreen Alqaisi <alqaisinisreen@gmail.com>
2018-11-18 14:44:13 -0800

    starter for user accounts code

---
+++ Bear-Down/app/controllers/home_controller.rb
@@ -0,0 +1,5 @@
+class HomeController < ActionController::Base
+	def index
+		render 'layouts/home'
+	end
+end
--- Bear-Down/config/routes.rb
+++ Bear-Down/config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :users, controllers: { sessions: 'users/sessions' }, path: 'auth', path_names: { sign_in: 'login', sign_out: 'logout', password: 'secret', confirmation: 'verification', unlock: 'unblock', registration: 'register', sign_up: 'cmon_let_me_in' }
+  root to: "home#index"
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
