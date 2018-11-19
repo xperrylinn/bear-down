24207889247cde4b27101dd8501b4f5777af96f3
Xavier <xperrylinn@berkeley.edu>
2018-11-19 12:28:15 -0800

    Working on setting up basic user authentication features

--- Bear-Down/config/routes.rb
+++ Bear-Down/config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: { sessions: 'users/sessions' }, path: 'auth', path_names: { sign_in: 'login', sign_out: 'logout', password: 'secret', confirmation: 'verification', unlock: 'unblock', registration: 'register', sign_up: 'cmon_let_me_in' }
+  devise_for :users, controllers: { sessions: 'users/sessions' }, path: 'auth'
   root to: "home#index"
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
