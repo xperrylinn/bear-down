bd336d66aee187e4d46f4cd66619e2b116dda593
Xavier <xperrylinn@berkeley.edu>
2018-11-21 09:07:04 -0800

    forgot to add routes.rb

--- Bear-Down/config/routes.rb
+++ Bear-Down/config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { sessions: 'users/sessions' }, path: 'auth'
   root to: "home#index"
+  get 'user_home', to: 'home#user_home', as: 'user_home'
+
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
